9edb8cf8734eadd9a6b55d6408ee8e79376a2e6a
clochard99 <egorushkina.katya@yandex.ru>
2022-11-15 16:48:01 +0100

    Sockets: adding threads

--- HMSWithABillingComponent/src/com/socket/Client.java
+++ HMSWithABillingComponent/src/com/socket/Client.java
@@ -1,18 +1,36 @@
 package com.socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private Socket s;
+    final Scanner sc = new Scanner(System.in);
     public Client(String address, int port) throws IOException {
         System.out.println("Connecting...");
         s = new Socket(address,port);
-        System.out.println("Congrats!");
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Send a message:");
+        Thread sender = new Thread(new Runnable() {
+            String message;
+            @Override
+            public void run() {
+                while(true) {
+                    message =sc.nextLine();
+                    out.println(message);
+                    out.flush();
+                }
+            }
+        });
+        sender.start();
     }
-
     public static void main(String[] args) throws IOException {
         Client client1 = new Client("localhost", 5699);
-        //Client client2 = new Client("null", 5699);
+
     }
 }
--- HMSWithABillingComponent/src/com/socket/Server.java
+++ HMSWithABillingComponent/src/com/socket/Server.java
@@ -1,16 +1,49 @@
 package com.socket;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     private ServerSocket ss;
+    final Scanner sc = new Scanner(System.in);
     public Server(int port) throws IOException {
         ss = new ServerSocket(port);
         System.out.println("Waiting for a client...");
-        ss.accept();
-        System.out.println("Success!");
+        Socket client = ss.accept();
+        System.out.println("A client has joined..");
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        //System.out.println("Client:" + "\u001B[35m" + exit + "\u001B[0m");
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+        Thread receiver = new Thread(new Runnable() {
+            String message;
+            @Override
+            public void run() {
+                try {
+                    message = in.readLine();
+                    while (message!=null) {
+                        System.out.println("Client:" + "\u001B[35m" + message + "\u001B[0m");
+                        message = in.readLine();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("A client has disconnected...");
+                out.close();
+                try {
+                    client.close();
+                    ss.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        receiver.start();
     }
 
     public static void main(String[] args) throws IOException {
