a92ca9d39c0e226c7f0be19dce186c8d8f944d9f
clochard99 <egorushkina.katya@yandex.ru>
2022-11-28 20:55:18 +0100

    Adding a new client

---
+++ HMSWithABillingComponent/src/com/socket/Client2.java
@@ -0,0 +1,36 @@
+package com.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client2 {
+    private Socket s;
+    final Scanner sc = new Scanner(System.in);
+    public Client2 (String address, int port) throws IOException {
+        System.out.println("Connecting...");
+        s = new Socket(address,port);
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Send a message:");
+        Thread sender = new Thread(new Runnable() {
+            String message;
+            @Override
+            public void run() {
+                while(true) {
+                    message =sc.nextLine();
+                    out.println(message);
+                }
+            }
+        });
+        sender.start();
+    }
+    public static void main(String[] args) throws IOException {
+        Client2 client1 = new Client2("localhost", 5699);
+
+    }
+}
+
